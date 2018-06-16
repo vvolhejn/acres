3feb6a3ac35cec7f296cb7f9814db6f1ba7c1542
Václav <vaclav.volhejn@gmail.com>
2018-06-16 23:34:54 +0200

    Create sublime project, no unicode in setup.py, __init__

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 setup(
     name="acres",
     version="0.1.0",
-    author="Václav Volhejn",
+    author="Vaclav Volhejn",
     description=("ML-based barcode sharpening."),
     license="MIT",
     packages=find_packages(),
